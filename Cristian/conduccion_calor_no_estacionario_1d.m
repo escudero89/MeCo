77a08998fb27f0ea86dcb2116e4c9b1664f8ed83
escudero89 <escudero89@gmail.com>
2013-09-14 01:29:13 -0300

    Adentro el backward euler

--- Cristian/conduccion_calor_no_estacionario_1d.m
+++ Cristian/conduccion_calor_no_estacionario_1d.m
@@ -16,6 +16,10 @@ function [PHI] = conduccion_calor_no_estacionario_1d(
 	% FORWARD EULER
 	if (tipo_df == 1)
  		PHI = fordward_euler(phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond, k, c, phi_amb, Q);
+	
+ 	% BACKWARD EULER
+ 	elseif (tipo_df == 2)
+ 		PHI = backward_euler(phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond, k, c, phi_amb, Q);
 	end
 
 
@@ -46,7 +50,7 @@ function [PHI] = fordward_euler(
 	h = dx;
 	h2 = h^2;
 
-	PHI = phi_0;
+	PHI = phi_0';
 	phi = phi_0;
 	phi_n = phi_0; % Esta usamos para guardar la variable phi sin sobreescribir
 
@@ -116,12 +120,113 @@ function [PHI] = fordward_euler(
 		phi = phi_n;
 
 		% Guardamos los phis como filas en la matriz
-		PHI = [PHI ; phi];
+		PHI = [PHI ; phi'];
 
 	end
 
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [PHI] = backward_euler(
+	phi_0,
+	Lx,
+	dx,
+	t_f,
+	dt,
+	tipo_cond,	% array dirichlet(0), neumann(1)
+	val_cond,		
+	k = @k_default,
+	c = @c_default,
+	phi_amb= @phi_amb_default,
+	Q = @Q_default
+ 	)
+	
+	% VARIABLES GENERICAS
+
+	xx = 0 : dx : Lx;
+	tt = 0 : dt : t_f;
+
+	% Redefino para usar h en vez de dx
+	h = dx;
+	h2 = h^2;
+
+	% Sea como fuere, voy a trabajar con length(phi_0) - 2 incognitas, dado que conozco los extremos
+	PHI = phi_0';
+	phi = phi_0;
+
+	begin = 1; % Begin, para utilizarlo junto con end
+
+	% No verificamos estabilidad, no es necesario
+
+	% Para agilizar, guardamos los vectores de k, c, phi_amb y Q
+	k_vec = phi;
+	c_vec = k_vec;
+	phi_amb_vec = k_vec;
+	Q_vec = k_vec;
+
+	% Avanzamos en todos los pasos del tiempo
+	for t = tt
+
+		% Lo definimos aca para reinicializarlo en cada dt
+		f = phi_0 * 0;
+		K = zeros(length(phi), 1);
+
+		for i = begin : length(f)
+			
+			% Usamos el n+1, por eso + d_t
+			k_vec(i) = k(xx(i), t + dt);
+			c_vec(i) = c(xx(i), t + dt);
+			phi_amb_vec(i) = phi_amb(xx(i), t + dt);
+			Q_vec(i) = Q(xx(i), t + dt);
+
+			% Armamos la matriz base K, y calculamos su inversa, para acelerar
+			K(i, i) = h2 / k_vec(i) * (1/dt + c_vec(i)) + 2;
+
+		end
+
+		% Armo f de forma vectorizada
+		f = h2 ./ k_vec .* (Q_vec + phi / dt + c_vec .* phi_amb_vec);
+
+		% La parte de arriba y la de abajo de la diagonal
+		K += diag(ones(length(phi) - 1, 1) * -1, 1) + diag(ones(length(phi) - 1, 1) * -1, -1);
+
+		% Revisamos los extremos
+		if (tipo_cond(begin) == 0)
+			K(begin, begin) = 1;
+			K(begin, begin + 1) = 0;
+			f(begin) = val_cond(begin);
+
+		% Antes las condiciones de neumann, en los extremos aparece un -2 en vez de un -1
+		elseif (tipo_cond(begin) == 1)
+			K(begin, begin + 1) = -2;
+			f(begin) += 2 * h / k_vec(i) * val_cond(begin);
+		end
+
+		% Otro extremo
+		if (tipo_cond(end) == 0)
+			K(end, end) = 1;
+			K(end, end - 1) = 0;
+			f(end) = val_cond(end);
+
+		elseif (tipo_cond(end) == 1)
+			K(end, end - 1) = -2;
+			f(end) += - 2 * h / k_vec(i) * val_cond(end);
+		end
+
+		K_inv = inv(K);
+
+		phi = K_inv * f;
+		
+		% Guardamos los phis como filas en la matriz
+		PHI = [PHI ; phi'];
+
+
+	end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Funciones por default                                                     %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [ret] = k_default(x_i, t_i)
--- Cristian/conduccion_calor_script.m
+++ Cristian/conduccion_calor_script.m
@@ -3,21 +3,30 @@ clear all;
 clf;
 close;
 
-tipo_df = 1;
 Lx = 1;
 dx = 0.1;
 t_f = 1;
 dt = 0.005;
-tipo_cond = [1 0];
-val_cond = [-1 0];
-phi_0 = zeros(1, length(0:dx:Lx));
+tipo_cond = [1 1];
+val_cond = [-1 10];
+phi_0 = zeros(length(0:dx:Lx), 1);
 
-[PHI] = conduccion_calor_no_estacionario_1d( tipo_df, phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond );
+[PHI_1] = conduccion_calor_no_estacionario_1d( 1, phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond );
+[PHI_2] = conduccion_calor_no_estacionario_1d( 2, phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond );
 
 cant_N = 10;
-N = cant_N * floor(size(PHI)(1) / cant_N);
+N = cant_N * floor(size(PHI_1)(1) / cant_N);
 
-plot(0:dx:Lx, PHI(1:cant_N:N,:));
+subplot (1, 2, 1)
+plot(0:dx:Lx, PHI_1(1:cant_N:N,:));
 grid;
 xlabel('x');
 ylabel('phi');
+title('Fordward Euler');
+ 
+subplot (1, 2, 2)
+plot(0:dx:Lx, PHI_2(1:cant_N:N,:));
+grid;
+xlabel('x');
+ylabel('phi');
+title('Backward Euler');

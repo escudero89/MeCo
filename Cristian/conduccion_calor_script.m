472c80216134d7144c20beec49ec2cab0ce217c6
escudero89 <escudero89@gmail.com>
2013-09-13 23:47:10 -0300

    add type of df

--- Cristian/conduccion_calor_script.m
+++ Cristian/conduccion_calor_script.m
@@ -3,6 +3,7 @@ clear all;
 clf;
 close;
 
+tipo_df = 1;
 Lx = 1;
 dx = 0.1;
 t_f = 1;
@@ -11,7 +12,7 @@ tipo_cond = [1 0];
 val_cond = [-1 0];
 phi_0 = zeros(1, length(0:dx:Lx));
 
-[PHI] = conduccion_calor_no_estacionario_1d( phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond );
+[PHI] = conduccion_calor_no_estacionario_1d( tipo_df, phi_0, Lx, dx, t_f, dt, tipo_cond, val_cond );
 
 cant_N = 10;
 N = cant_N * floor(size(PHI)(1) / cant_N);

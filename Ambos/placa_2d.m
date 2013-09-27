3ee1cd775d58fd68dbcf914ba5aaae40909d6f9d
escudero89 <escudero89@gmail.com>
2013-09-26 23:36:56 -0300

    adentro nodos interiores

--- Ambos/placa_2d.m
+++ Ambos/placa_2d.m
@@ -1,47 +1,49 @@
-## Copyright (C) 2013 Marcos
-## 
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+%% Copyright (C) 2013 Marcos
+%% 
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%% 
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%% 
+%% You should have received a copy of the GNU General Public License
+%% along with Octave; see the file COPYING.  If not, see
+%% <http://www.gnu.org/licenses/>.
 
-## conduccionCalor_estacionario_2d
+%% conduccionCalor_estacionario_2d
 
-## Author: Marcos <marcos@marcosNetbook> y Cristian tambien che :(
-## Created: 2013-09-04
+%% Author: Marcos <marcos@marcosNetbook> y Cristian tambien che :(
+%% Created: 2013-09-04
 
 function [ phi ] = placa_2d (
-    cant_x,         # Cantidad de nodos en x
-    cant_y,         # Cantidad de nodos en y
-    Lx,             # Tamanho del rectangulo en el eje x
-    Ly,             # Tamanho del rectangulo en el eje y
-    q,              # Carga distribuida
-    D,              # Rigidez a la flexion
-    cond_contorno,  # Un array que indica si estamos ante soporte simple (0), empotrado (1), libre (2).
+    cant_x,         % Cantidad de nodos en x
+    cant_y,         % Cantidad de nodos en y
+    Lx,             % Tamanho del rectangulo en el eje x
+    Ly,             % Tamanho del rectangulo en el eje y
+    q,              % Carga distribuida
+    D,              % Rigidez a la flexion
+    cond_contorno,  % Un array que indica si estamos ante soporte simple (0), empotrado (1), libre (2).
     valor_cc_1,         
     valor_cc_2,
     valor_cc_3,
     valor_cc_4
     )
 
-    ## Obtencion de variables
+    begin = 1;
+
+    %% Obtencion de variables
     dx = Lx / (cant_x - 1);
     dy = Ly / (cant_y - 1);
     
     dx2 = dx^2;
-    dx4 = dx^4;
+    dx4 = dx^4
     
     dy2 = dy^2;
-    dy4 = dy^4;
+    dy4 = dy^4
 
     phi = zeros(cant_x * cant_y, 1);
     f =  phi;
@@ -50,50 +52,121 @@ function [ phi ] = placa_2d (
     x = 0 : dx : Lx;
     y = 0 : dy : Ly;
     
-    ## Definimos direccion de etiquetado de nodos
+    %% PARA OPTIMIZAR el calculo de nodos interiores
+
+    A_interior = 6 * (dy4 + dx4) + 8 * dx2 * dy2;
+    B_interior = 2 * dx2 * dy2;
+    Cx_interior = -4 * (dx4 + dx2 * dy2);
+    Cy_interior = -4 * (dy4 + dx2 * dy2);
+
+    %% Definimos direccion de etiquetado de nodos
     if (cant_x > cant_y)
         ir_dir_y = true;
         cant_nodo_en_dir = cant_y;
     else
         ir_dir_y = false;
         cant_nodo_en_dir = cant_x;
-    endif
+    end
+
+    % Generamos un vector que nos dice si un nodo es interior (1), o de borde ext (-1) o int(0)
+    es_interior = ones(length(phi), 1);
+
+    % Lo armamos como matriz
+    ES_INTERIOR = reshape(es_interior, cant_y, cant_x)
+
+    % Llenamos los bordes exteriores
+    ES_INTERIOR(begin, :) = -1;
+    ES_INTERIOR(:, begin) = -1;
+    ES_INTERIOR(:, end) = -1;
+    ES_INTERIOR(end, :) = -1;
+
+    % Y los bordes interiores
+    ES_INTERIOR(begin + 1, 2 : cant_x - 1) = 0;
+    ES_INTERIOR(2 : cant_y - 2, begin + 1) = 0;
+    ES_INTERIOR(2 : cant_y - 2, end - 1) = 0;
+    ES_INTERIOR(end - 1, 2 : cant_x - 1) = 0;
+
+    % Esquinas en orden antihorario
+    esquina = [21 22 23 24];
+
+    % Asignamos -21 -22 -23 -24 a los nodos borde-exteriores esquinas en sentido horario.
+    ES_INTERIOR(begin, begin) = -esquina(4);
+    ES_INTERIOR(begin, end) = -esquina(3);
+    ES_INTERIOR(end, end) = -esquina(2);
+    ES_INTERIOR(end, begin) = -esquina(1);
+
+    % Asignamos 21, 22, 23, 24 a los nodos borde-interior esquinas en sentido horario
+    ES_INTERIOR(begin + 1, begin + 1) = esquina(4);
+    ES_INTERIOR(begin + 1, end - 1) = esquina(3);
+    ES_INTERIOR(end - 1, begin + 1) = esquina(1);
+    ES_INTERIOR(end - 1, end - 1) = esquina(2);
+
+    % Asignamos un valor especial a los nodos vecinos a los de las esquinas
+    ES_INTERIOR(begin + 1, begin) = -esquina(4) * 5;
+    ES_INTERIOR(begin, begin + 1) = -esquina(4) * 7;
     
-    # Posicion actual en la malla (como vector)
-    pos_en_malla = 1;
-    
-    # Salteo los nodos del borde interior y exterior
-    pos_en_malla += 2 * cant_nodo_en_dir + 1;
-    
-    # Generamos un vector que nos dice si un nodo es interior (1), o de borde ext-int (0)
-    es_interior = zeros(length(phi), 1);
-    
-    # Hasta length(phi) - cant_nodo_en_dir porque salteamos la ultima parte que son nodos de borde.
-    for pos = pos_en_malla : length(phi) - 2 * cant_nodo_en_dir - 1
-        
-        if (mod(pos, cant_nodo_en_dir) > 1)
-            es_interior(pos) = 1;
-        endif
-        
-    endfor
-    
-    
-    return;
-    # Asignamos 21 22 23 24 a los nodos  esquinas en sentido horario.
-    if(ir_dir_y)
-        es_interior(1) = 21;
-        es_interior(cant_nodo_en_dir) = 22;
-        es_interior(end - cant_nodo_en_dir + 1) = 24;
-        es_interior(end) = 23;
-    else
-        es_interior(1) = 21;
-        es_interior(cant_nodo_en_dir) = 24;
-        es_interior(end - cant_nodo_en_dir + 1) = 22;
-        es_interior(end) = 23;
-    
-    endif
-    # Cambiamos dx2 dy2 dependiendo de la direccion y lo traajamos en forma
-    # transparente con da db
+    ES_INTERIOR(begin + 1, end) = -esquina(3) * 5;
+    ES_INTERIOR(begin, end - 1) = -esquina(3) * 7;
+
+    ES_INTERIOR(end - 1, begin) = -esquina(1) * 5;
+    ES_INTERIOR(end, begin + 1) = -esquina(1) * 7;
+
+    ES_INTERIOR(end - 1, end) = -esquina(2) * 5;
+    ES_INTERIOR(end, end - 1) = -esquina(2) * 7;
+
+    ES_INTERIOR
+
+    % Retornamos a la forma de vector
+    es_interior = reshape(ES_INTERIOR, cant_x * cant_y, 1);
+
+    % NOTAR que no es i, j; es j, i los indices, pero para pararse en la matriz es i,j
+    for i = 1 : cant_y
+        for j = 1 : cant_x
+
+            % Posicion actual en formato vector
+            pos_vec = coord2vec(j, i, ir_dir_y, cant_nodo_en_dir);
+            
+            f(pos_vec) = q * D;
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % Si hay un 1, es porque es un nodo interior: stencil de 8 puntos.
+            if (ES_INTERIOR(i, j) == 1)
+
+                % Esta matriz la voy a convertir en fila y se la asigno a K
+                Z = zeros(cant_y, cant_x);
+
+                Z(i, j) = A_interior;
+            
+                Z(i + 1, j + 1) = Z(i + 1, j - 1) = Z(i - 1, j + 1) = Z(i - 1, j - 1) = B_interior;
+
+                Z(i + 2, j) = Z(i - 2, j) = dy4;
+                Z(i, j + 2) = Z(i, j - 2) = dx4;
+
+                Z(i + 1, j) = Z(i - 1, j) = Cy_interior;
+                Z(i, j + 1) = Z(i, j - 1) = Cx_interior;
+
+                % Si avanzamos en x, lo transponemos (el reshape es por columnas)
+                if (!ir_dir_y)
+                    Z = Z'
+                end
+
+                % Reshape a vector
+                Z_vec = reshape(Z, 1, cant_x * cant_y);    
+                
+                % Y lo guardamos en la matriz en su respectiva fila
+                K(pos_vec, :) = Z_vec;
+
+            end
+
+        end
+    end
+    K
+reshape(K(es_interior==1,:)(1, :), cant_y, cant_x)
+return;
+
+
+    % Cambiamos dx2 dy2 dependiendo de la direccion y lo traajamos en forma
+    % transparente con da db
     if(ir_dir_y)
         da2 = dy2;
         db2 = dx2;
@@ -104,11 +177,11 @@ function [ phi ] = placa_2d (
         signo = -1;
     endif
     
-    # Armamos la matriz k diagonal en bordes.
+    % Armamos la matriz k diagonal en bordes.
     for i = 1 : length(es_interior)
 
-        ###########################################################################
-        # Si hay un 1, es porque es un nodo interior: tomamos cartas en el asunto.
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Si hay un 1, es porque es un nodo interior: tomamos cartas en el asunto.
         if (es_interior(i) == 1)
             
             K(i, i) = C / k - 2 * (db2 + da2) / (db2 * da2);
@@ -119,8 +192,8 @@ function [ phi ] = placa_2d (
             K(i, i - cant_nodo_en_dir) = 1 / db2;
             K(i, i + cant_nodo_en_dir) = 1 / db2;
  
-        ###########################################################################       
-        #Si es mayor a 20 es xq es una esquina                
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       
+        %Si es mayor a 20 es xq es una esquina                
         elseif ( es_interior(i) > 20 ) 
         
             K(i, i) = C / k + 2 * (dx2 + dy2) / (dx2 * dy2);
@@ -167,11 +240,11 @@ function [ phi ] = placa_2d (
                     
             endif
         
-        ###########################################################################
-        # Nodo de borde (es_interior es 0). cond_contorno = 0 dirichlet , 1 neumann
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Nodo de borde (es_interior es 0). cond_contorno = 0 dirichlet , 1 neumann
         else
         
-            # El vector idx se utiliza para numerar los distintos contornos dependiendo de la direccion.
+            % El vector idx se utiliza para numerar los distintos contornos dependiendo de la direccion.
             idx = [11 14 12 13];
             
             if (ir_dir_y == 0)
@@ -180,7 +253,7 @@ function [ phi ] = placa_2d (
             
             endif
             
-            if (i < cant_nodo_en_dir) ## Borde 1 o 4
+            if (i < cant_nodo_en_dir) %% Borde 1 o 4
             
                 es_interior(i) = idx(1);
             
@@ -204,19 +277,19 @@ function [ phi ] = placa_2d (
 
         [l, m] = vec2coord(i, ir_dir_y, cant_nodo_en_dir)
 
-        # Si es nodo interior o de esquina
+        % Si es nodo interior o de esquina
         if (es_interior(i) == 1 || es_interior(i) > 20)
                         
                 f(i) = 1 / k * (C * phi_amb) - Q(x(l), y(m)) / k;
 
-        # nodo de borde no esquina
+        % nodo de borde no esquina
         else
 
-            K(i,i) = C/k - 2 * (dx2 + dy2)/(dx2 * dy2); #igual para todos los bordes
+            K(i,i) = C/k - 2 * (dx2 + dy2)/(dx2 * dy2); %igual para todos los bordes
             f(i) = -Q(x(l), y(m)) / k;
               
             if(es_interior(i) == 11)
-                # Si es 1, es neumann
+                % Si es 1, es neumann
                 if (cond_contorno(1))
                         
                     K(i, coord2vec(l+1, m, ir_dir_y, cant_nodo_en_dir) ) = 2/dx2;
@@ -225,7 +298,7 @@ function [ phi ] = placa_2d (
 
                     f(i) += 1/k * (C * phi_amb - 2 * valor_cc_1(m) / dx);
                 
-                else # Dirichlet
+                else % Dirichlet
                 
                     K(i, i) = 1;
                     f(i) = valor_cc_1(m);
@@ -294,12 +367,12 @@ function [ phi ] = placa_2d (
     f
     phi = K \ f
     
-    ## PLOTEO
+    %% PLOTEO
     
-    # Generamos malla
+    % Generamos malla
     [ XX , YY ] = meshgrid(x, y);
     
-    # Mapeamos punto phi a coordenada x,y
+    % Mapeamos punto phi a coordenada x,y
     i = 1;
     PHI = [];
     length(phi)
@@ -307,7 +380,7 @@ function [ phi ] = placa_2d (
     
         if (ir_dir_y)
             PHI = [PHI , phi(i : i + cant_nodo_en_dir - 1)];
-            # PHI = reshape(phi, cant_nodo_en_dir, prod(length(phi) / cant_nodo_en_dir);
+            % PHI = reshape(phi, cant_nodo_en_dir, prod(length(phi) / cant_nodo_en_dir);
         else
             PHI = [PHI ; phi(i : i + cant_nodo_en_dir - 1)'];
         endif
@@ -327,16 +400,16 @@ endfunction
 
 function [pos] = coord2vec( i, j, ir_dir_y, cant_nodo_en_dir)
 
-    if(ir_dir_y)
+    if (ir_dir_y)
     
         pos = j + cant_nodo_en_dir * (i - 1);
     
     else
         pos = i + cant_nodo_en_dir * (j - 1);
     
-    endif
+    end
 
-endfunction
+end
 
 function [l, m] = vec2coord(i, ir_dir_y, cant_nodo_en_dir)
 
--- Ambos/placa_2d_script.m
+++ Ambos/placa_2d_script.m
@@ -1,32 +1,29 @@
 1;
 
-cant_x = 14;         # Cantidad de nodos en x
-cant_y = 15;         # Cantidad de nodos en y
-Lx = 3;             # Tamanho del rectangulo en el eje x
-Ly = 3;          # Tamanho del rectangulo en el eje y
-Q = @Q_2d;              # Fuente
-k = 1;              # La conductividad termica
-C = 0;              # El calor especifico
-phi_amb = 0;        # El valor de phi del ambiente
+cant_x = 6;         % Cantidad de nodos en x
+cant_y = 5;         % Cantidad de nodos en y
+Lx = 3;             % Tamanho del rectangulo en el eje x
+Ly = 3;          % Tamanho del rectangulo en el eje y
+q = 1;              % Carga distribuida
+D = 1;              % Rigidez a la flexion
 
-cond_contorno = [0 1 1 0];  # Un array que indica si estamos ante dirichlet (0) o neumann (1)
+cond_contorno = [0 1 1 0];  % Un array que indica si estamos ante dirichlet (0) o neumann (1)
 valor_cc_1 = 100 * ones(cant_y,1);
 valor_cc_2 = 0 * ones(cant_x,1); 
 valor_cc_3 = 0 * ones(cant_y,1);
 valor_cc_4 = 100 * ones(cant_x,1);
 
-[ phi ] = conduccionCalor_estacionario_2d (
-    cant_x,         # Cantidad de nodos en x
-    cant_y,         # Cantidad de nodos en y
-    Lx,             # Tamanho del rectangulo en el eje x
-    Ly,             # Tamanho del rectangulo en el eje y
-    Q,              # Fuente
-    k,              # La conductividad termica
-    C,              # El calor especifico
-    phi_amb,        # El valor de phi del ambiente
-    cond_contorno,  # Un array que indica si estamos ante dirichlet (0) o neumann (1)
-    valor_cc_1,
+[ phi ] = placa_2d (
+    cant_x,         % Cantidad de nodos en x
+    cant_y,         % Cantidad de nodos en y
+    Lx,             % Tamanho del rectangulo en el eje x
+    Ly,             % Tamanho del rectangulo en el eje y
+    q,              % Carga distribuida
+    D,              % Rigidez a la flexion
+    cond_contorno,  % Un array que indica si estamos ante soporte simple (0), empotrado (1), libre (2).
+    valor_cc_1,         
     valor_cc_2,
     valor_cc_3,
     valor_cc_4
     )
+
